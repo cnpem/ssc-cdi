3d9858429c8cb70b22f0992074b4349ea920ce36
Yuri <yuri.tonin@lnls.br>
2022-04-19 11:23:40 -0300

    Added fuction for GUI that selects fresnel number

--- sscCdi/caterete/fresnel.py
+++ sscCdi/caterete/fresnel.py
@@ -15,6 +15,9 @@ import numpy as np
 from moviepy.editor import VideoClip, ImageSequenceClip
 from moviepy.video.io.bindings import mplfig_to_npimage
  
+import ipywidgets as widgets 
+from ipywidgets import fixed
+
 def plotshow(imgs, f1,file, legending_f_value=[],cmap='jet',nlines=1, bLog = False, interpolation='bilinear'): # legending_f_value = plot titles
         num = len(imgs)
 
@@ -101,6 +104,95 @@ def create_propagation_video(path_to_probefile,
             clip.write_gif('propagation.gif', fps=frame_rate)
 
     return image_list, f1
+
+
+
+centered_box_layout = widgets.Layout(flex_flow='column',align_items='center',width='30%')
+centered_button_layout = widgets.Layout(flex_flow='column',align_items='center',width='30%',height='50px')
+
+power   = widgets.BoundedFloatText(value=-4,min=-10,max=0, description='10^n', readout_format='d',disabled=False,layout=centered_box_layout)
+start_f = widgets.FloatText(value=1, description='Start F1', readout_format='d',disabled=False,layout=centered_box_layout)
+end_f   = widgets.FloatText(value=9, description='End F1', readout_format='d',disabled=False,layout=centered_box_layout)
+n_frames= widgets.FloatText(value=20, description='Number of Frames', readout_format='d',disabled=False,layout=centered_box_layout)
+
+def update_values(n_frames,start_f,end_f,power):
+    global starting_f_value
+    global ending_f_value
+    global number_of_frames
+    starting_f_value=-start_f*10**power
+    ending_f_value=-end_f*10**power
+    number_of_frames=int(n_frames)
+
+
+def update_imshow(fig,ax1,image_list,f1_list,index):
+    print('Plotting new probe propagation...')
+    ax1.clear()
+    ax1.set_title(f'f1 = {f1_list[index]:.2e}')
+    ax1.imshow(image_list[index],cmap='jet')
+    fig.canvas.draw_idle()
+    
+
+def update_dic(output_dictionary,key,boxvalue):
+    output_dictionary[key]  = boxvalue 
+
+def on_click_propagate(dummy,args=()):
+ 
+    path_to_probefile,starting_f_value,ending_f_value,number_of_frames,play,slider,fig, ax1 = args
+
+    image_list, f1_list = create_propagation_video(path_to_probefile,
+                            starting_f_value=starting_f_value,
+                            ending_f_value=ending_f_value,
+                            number_of_frames=number_of_frames,
+                            jupyter=True)
+
+    play.max = len(image_list)-1
+    slider.max = len(image_list)-1
+
+    out = widgets.interactive_output(update_imshow,{'fig':fixed(fig),'ax1':fixed(ax1),'image_list':fixed(image_list),'f1_list':fixed(f1_list),'index':play})
+    display(out)
+
+
+def deploy_interface_fresnel(path_to_probefile,output_dictionary):
+
+    from functools import partial
+
+    out2 = widgets.interactive_output(update_values,{'n_frames':n_frames,'start_f':start_f,'end_f':end_f,'power':power})
+    display(widgets.HBox([n_frames, power, start_f,end_f]),out2)
+
+    start_ptycho_button = widgets.Button(description=('Propagate Probe'),layout=centered_button_layout)
+    output = widgets.Output()
+    display(start_ptycho_button,output)
+
+    """ Fresnel Number box """
+    fresnel_box = widgets.FloatText(value=-5e-3, description='Chosen F (float)', readout_format='.3e',disabled=False,layout=centered_box_layout)
+    widgets.interactive_output(update_dic,{'output_dictionary':fixed(output_dictionary),'key':fixed('f1'),'boxvalue':fresnel_box})
+
+    image_list, f1_list = [np.ones((100,100))], [0]
+
+    play = widgets.Play(
+        value=0,
+        min=0,
+        max=len(image_list)-1,
+        step=1,
+        interval=300,
+        description="Press play",
+        disabled=False
+    )
+
+    slider = widgets.IntSlider(value=0, min=0, max=len(image_list)-1)
+    play_box = widgets.HBox([play, slider])
+    widgets.jslink((play, 'value'), (slider, 'value'))
+    display(widgets.HBox([play_box,fresnel_box]))
+
+
+    fig = plt.figure(figsize=(5,5))
+    ax1  = fig.add_subplot(1, 1, 1)
+    ax1.imshow(image_list[0],cmap='jet') # initialize
+    
+    args = (path_to_probefile,starting_f_value,ending_f_value,number_of_frames,play,slider,fig,ax1)
+    start_ptycho_button.on_click(partial(on_click_propagate,args=args))
+
+    return fig, ax1
 
 if __name__ == '__main__':
     
