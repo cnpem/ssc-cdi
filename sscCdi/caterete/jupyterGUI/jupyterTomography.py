0bf077b6c3356168c5764ec6e7eb711e82f193db
yuri.tonin <yuri.tonin@lnls.br>
2022-05-05 11:15:29 -0300

    Finished Phase Unwrap tab

--- sscCdi/caterete/jupyterGUI/jupyterTomography.py
+++ sscCdi/caterete/jupyterGUI/jupyterTomography.py
@@ -33,9 +33,23 @@ global_dict = {"ibira_data_path": "path/to/ibira/difpads",
 
 output_folder = global_dict["sinogram_path"].rsplit('/',1)[0]  #os.path.join(global_dict["ibira_data_path"], 'proc','recons',global_dict["folders_list"][0]) # changes with control
 
+class VideoControl:
+    
+    def __init__ (self,slider,value,minimum,maximum,step,interval,description):
+    
+        self.widget = widgets.Play(value=value,
+                            min=minimum,
+                            max=maximum,
+                            step=step,
+                            interval=interval,
+                            description=description,
+                            disabled=False )
+
+        widgets.jslink((self.widget, 'value'), (slider, 'value'))
+
 class Button:
 
-    def __init__(self,description="DESCRIPTION",width="20%",height="30px",icon=""):
+    def __init__(self,description="DESCRIPTION",width="50%",height="50px",icon=""):
 
         self.button_layout = widgets.Layout(width=width, height=height)
         self.widget = widgets.Button(description=description,layout=self.button_layout,icon=icon)
@@ -94,12 +108,56 @@ class Input(object):
         else:
             self.dictionary[self.key] = value            
 
+import asyncio
+
+class Timer:
+    def __init__(self, timeout, callback):
+        self._timeout = timeout
+        self._callback = callback
+
+    async def _job(self):
+        await asyncio.sleep(self._timeout)
+        self._callback()
+
+    def start(self):
+        self._task = asyncio.ensure_future(self._job())
+
+    def cancel(self):
+        self._task.cancel()
+
+def debounce(wait):
+    """ Decorator that will postpone a function's
+        execution until after `wait` seconds
+        have elapsed since the last time it was invoked. """
+    def decorator(fn):
+        timer = None
+        def debounced(*args, **kwargs):
+            nonlocal timer
+            def call_it():
+                fn(*args, **kwargs)
+            if timer is not None:
+                timer.cancel()
+            timer = Timer(wait, call_it)
+            timer.start()
+        return debounced
+    return decorator
             
 def folders_tab():
+
+    output = widgets.Output()
+    
+    def sort_frames(dummy):
+        with output:
+            print('Still need to implement sorting function!')
+    
     ibira_data_path = Input(global_dict,"ibira_data_path")
     folders_list    = Input(global_dict,"folders_list")
     sinogram_path   = Input(global_dict,"sinogram_path")
-    box = widgets.VBox([ibira_data_path.widget,folders_list.widget,sinogram_path.widget])
+    
+    sort_button = Button(description="Sort frames",icon="fa-sort-numeric-asc")
+    sort_button.trigger(sort_frames)
+    
+    box = widgets.VBox([ibira_data_path.widget,folders_list.widget,sinogram_path.widget,sort_button.widget,output])
     return box
 
 
@@ -119,9 +177,10 @@ def crop_tab():
     
     def load_frames(dummy, args = ()):
         global sinogram
-        top_crop, bottom_crop, left_crop, right_crop, select_slider = args
+        top_crop, bottom_crop, left_crop, right_crop, select_slider, play_control = args
         sinogram = np.load(global_dict["sinogram_path"])
         select_slider.widget.max, select_slider.widget.value = sinogram.shape[0]-1, sinogram.shape[0]//2
+        play_control.widget.max = select_slider.widget.max
         vertical_max, horizontal_max = sinogram.shape[1]//2, sinogram.shape[2]//2
         top_crop.widget.max  = bottom_crop.widget.max = sinogram.shape[1]//2 - 1
         left_crop.widget.max = right_crop.widget.max  = sinogram.shape[2]//2 - 1
@@ -139,15 +198,18 @@ def crop_tab():
     right_crop    = Input(global_dict,"right_crop" ,description="Right", bounded=(1,horizontal_max,1),slider=True)
     select_slider = Input({"dummy_key":1},"dummy_key",description="Select Frame", bounded=(0,100,1),slider=True)
 
+    play_control = VideoControl(select_slider.widget,select_slider.widget.value,select_slider.widget.min,select_slider.widget.max,1,300,"Play Button")
+    play_box = widgets.HBox([select_slider.widget,play_control.widget])
+    
     load_frames_button  = Button(description="Load Frames",width='50%', height='50px',icon='fa-file-o')
-    args = (top_crop, bottom_crop, left_crop, right_crop, select_slider)
+    args = (top_crop, bottom_crop, left_crop, right_crop, select_slider, play_control)
     load_frames_button.trigger(partial(load_frames,args=args))
 
     save_cropped_frames_button = Button(description="Save cropped frames",width='70%', height='50px',icon='fa-floppy-o') 
     args2 = (top_crop.widget,bottom_crop.widget,left_crop.widget,right_crop.widget)
     save_cropped_frames_button.trigger(partial(save_cropped_sinogram,args=args2))
     
-    sliders_box = widgets.VBox([load_frames_button.widget,select_slider.widget,top_crop.widget,bottom_crop.widget,left_crop.widget,right_crop.widget,save_cropped_frames_button.widget])
+    sliders_box = widgets.VBox([load_frames_button.widget,play_box,top_crop.widget,bottom_crop.widget,left_crop.widget,right_crop.widget,save_cropped_frames_button.widget])
     box = widgets.HBox([sliders_box,output])
     return box
 
@@ -168,8 +230,13 @@ def update_image(image):
     subplot.clear()
     subplot.imshow(image,cmap='gray')
     figure.canvas.draw_idle()
+
+
     
 def unwrap_tab():
+    
+    global unwrapped_sinogram
+    unwrapped_sinogram = np.empty_like(sinogram)
     
     output = widgets.Output()
     with output:
@@ -183,33 +250,62 @@ def unwrap_tab():
         plt.show()
     
     def phase_unwrap(dummy):
+        global unwrapped_sinogram
         with output: print('Performing phase unwrap...')
         unwrapped_sinogram = unwrap_in_parallel(cropped_sinogram,iterations_slider.widget.value,non_negativity=non_negativity_checkbox.widget.value,remove_gradient = gradient_checkbox.widget.value)
         widgets.interactive_output(update_imshow, {'sinogram':fixed(unwrapped_sinogram),'figure':fixed(figure_unwrap),'subplot':fixed(subplot_unwrap[1]),'top': fixed(0), 'bottom': fixed(None), 'left': fixed(0), 'right': fixed(None), 'frame_number': selection_slider.widget})    
         
     def load_cropped_frames(dummy,args=()):
         global cropped_sinogram
-        selection_slider = args
+        selection_slider, play_control = args
         cropped_sinogram = np.load(os.path.join(output_folder,'cropped_sinogram.npy'))
         selection_slider.widget.max, selection_slider.widget.value = cropped_sinogram.shape[0] - 1, cropped_sinogram.shape[0]//2
+        play_control.widget.max =  selection_slider.widget.max
         widgets.interactive_output(update_imshow, {'sinogram':fixed(cropped_sinogram),'figure':fixed(figure_unwrap),'subplot':fixed(subplot_unwrap[0]),'top': fixed(0), 'bottom': fixed(None), 'left': fixed(0), 'right': fixed(None), 'frame_number': selection_slider.widget})    
-        
+
+    def correct_bad_frames(dummy,bad_frames1=[],bad_frames2=[]):
+        bad_frames = bad_frames1 + bad_frames2 # concatenate lists
+        with output: print('Zeroing frames: ', bad_frames)
+        cropped_sinogram[bad_frames,:,:]   = np.zeros((cropped_sinogram.shape[1],cropped_sinogram.shape[2]))
+        unwrapped_sinogram[bad_frames,:,:] = np.zeros((cropped_sinogram.shape[1],cropped_sinogram.shape[2]))
+
+    @debounce(0.5) # check changes every 0.5sec
+    def update_lists(bad_frames_list1,bad_frames_list2):
+        bad_frames_list1 = ast.literal_eval(bad_frames_list1)
+        bad_frames_list2 = ast.literal_eval(bad_frames_list2)
+        correct_bad_frames_partial = partial(correct_bad_frames,bad_frames1=bad_frames_list1,bad_frames2=bad_frames_list2)
+        correct_bad_frames_button.trigger(correct_bad_frames_partial)
+   
+    def save_sinogram(dummy,sinogram_to_save=np.ones((2,2,2)),filename="dummy.npy"):
+        np.save(os.path.join(output_folder,filename),sinogram_to_save)
+        with output: print('Saved sinogram at: ',os.path.join(output_folder,filename))
+
     load_cropped_frames_button = Button(description="Load cropped frames",width='50%', height='50px',icon='fa-file-o')
 
     bad_frames_list = Input(global_dict,"bad_frames_list", description = 'Bad frames',layout=widgets.Layout(align_items='flex-start',width='80%'))
     bad_frames_list2 = Input(global_dict,"bad_frames_list2",description='Bad Frames after Unwrap',layout=widgets.Layout(align_items='flex-start',width='80%'))
+    widgets.interactive_output(update_lists,{ "bad_frames_list1":bad_frames_list.widget,"bad_frames_list2":bad_frames_list2.widget})
+    
     iterations_slider = Input(global_dict,"unwrap_iterations",bounded=(0,10,1),slider=True, description='Iterations')
     non_negativity_checkbox = Input(global_dict,"unwrap_non_negativity",layout=widgets.Layout(align_items='flex-start',width='40%'),description='Non-negativity')
     gradient_checkbox = Input(global_dict,"unwrap_gradient_removal",layout=widgets.Layout(align_items='flex-start',width='40%'),description='Gradient')
-    selection_slider = Input({"dummy_key":1},"dummy_key",description="Select Frame", bounded=(0,10,1),slider=True)
     preview_unwrap_button = Button(description="Preview unwrap",width='50%', height='50px',icon='play')
     preview_unwrap_button.trigger(phase_unwrap)
     
-    args = (selection_slider)
+    selection_slider = Input({"dummy_key":1},"dummy_key",description="Select Frame", bounded=(0,10,1),slider=True)
+    play_control = VideoControl(selection_slider.widget,selection_slider.widget.value,selection_slider.widget.min,selection_slider.widget.max,1,300,"Play Button")
+    play_box = widgets.HBox([selection_slider.widget,play_control.widget])
+    
+    args = (selection_slider,play_control)
     load_cropped_frames_button.trigger(partial(load_cropped_frames,args=args))
 
+    correct_bad_frames_button = Button(description='Correct Bad Frames',icon='fa-check-square-o')
+    
+    save_unwrapped_button = Button(description="Save unwrapped frames",icon='fa-floppy-o') 
+    save_unwrapped_button.trigger(partial(save_sinogram,sinogram_to_save=unwrapped_sinogram,filename='unwrapped_sinogram.npy'))
+    
     unwrap_params_box = widgets.VBox([iterations_slider.widget,non_negativity_checkbox.widget,gradient_checkbox.widget])
-    controls_box = widgets.VBox([load_cropped_frames_button.widget,preview_unwrap_button.widget,selection_slider.widget, unwrap_params_box,bad_frames_list.widget,bad_frames_list2.widget])
+    controls_box = widgets.VBox([load_cropped_frames_button.widget,correct_bad_frames_button.widget,preview_unwrap_button.widget,save_unwrapped_button.widget,play_box, unwrap_params_box,bad_frames_list.widget,bad_frames_list2.widget])
     plot_box = widgets.VBox([output])
         
     box = widgets.HBox([controls_box,plot_box])
@@ -222,8 +318,8 @@ def deploy_tabs(tab1=folders_tab(),tab2=crop_tab(),tab3=unwrap_tab()):
     "Select Folders" : tab1,
     "Cropping"       : tab2,
     "Phase Unwrap"   : tab3,
-    "Wiggle"         : widgets.Text(description="Convex Hull"),
-    "Regularization" : widgets.Text(description="Wiggle"),
+    "Convex Hull"         : widgets.Text(description="Convex Hull"),
+    "Wiggle" : widgets.Text(description="Wiggle"),
     "Tomography"     : widgets.Text(description="Tomography")}
     
     button_layout = widgets.Layout(width='30%', height='100px',max_height='50px')
