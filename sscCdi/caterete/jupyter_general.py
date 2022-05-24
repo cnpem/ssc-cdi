61d8c2f2be381583cbb927754a954ce1ad7501c6
yuri.tonin <yuri.tonin@lnls.br>
2022-05-24 16:43:34 -0300

    Minor changes to packages installation

--- sscCdi/caterete/jupyter_general.py
+++ sscCdi/caterete/jupyter_general.py
@@ -1,10 +1,29 @@
 import subprocess
 
-def install_packages():
+def install_packages(mafalda):
+    print('Installing packages @ Bertha (local)...')
     cmd = """pip config --user set global.extra-index-url http://gcc.lnls.br:3128/simple/
     pip config --user set global.trusted-host gcc.lnls.br
     pip install sscCdi==0.0.5
     pip install sscPimega==0.0.4
     pip install sscRaft==1.0.2
     pip install sscResolution==1.2.3"""
-    subprocess.call(cmd, shell=True)
+    for line in cmd.split('\n'):
+        subprocess.call(line, shell=True)
+    print('\t Done!')
+
+    # print('Installing packages @ Mafalda (cluster)...')
+    # cmd = """module load python3/3.9.2
+    # module load cuda/11.2
+    # module load hdf5/1.12.0_parallel
+    # pip config --user set global.extra-index-url http://gcc.lnls.br:3128/simple/
+    # pip config --user set global.trusted-host gcc.lnls.br
+    # pip install sscCdi==0.0.5
+    # pip install sscPimega==0.0.4
+    # pip install sscRaft==1.0.2
+    # pip install sscResolution==1.2.3"""
+    # for line in cmd.split('\n'):
+    #     stdin, stdout, stderr = mafalda.exec_command(line)
+    #     print('Output: ',stdout.read())
+    #     print('Error:  ',stderr.read())   
+    # print('\t Done!')
