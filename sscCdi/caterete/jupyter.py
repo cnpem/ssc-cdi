0de1b2e098b9a28a39eaaabacd4f739576e650bc
yuri.tonin <yuri.tonin@lnls.br>
2022-06-10 15:46:48 -0300

    updated function that install packages

--- sscCdi/caterete/jupyter.py
+++ sscCdi/caterete/jupyter.py
@@ -67,10 +67,11 @@ def install_packages(mafalda):
     print('Installing packages @ Bertha (local)...')
     cmd = """pip config --user set global.extra-index-url http://gcc.lnls.br:3128/simple/
 pip config --user set global.trusted-host gcc.lnls.br
-pip install sscCdi==0.0.5
-pip install sscPimega==0.0.4
-pip install sscRaft==1.0.0
-pip install sscResolution==1.2.3"""
+pip install --upgrade --force-reinstall sscCdi==0.1.0
+pip install --upgrade --force-reinstall sscPimega==0.0.4
+pip install --upgrade --force-reinstall sscRaft==1.0.3
+pip install --upgrade --force-reinstall sscRadon==1.0.0
+pip install --upgrade --force-reinstall sscResolution==1.2.3"""
     for line in cmd.split('\n'):
         print('\t',line)
         subprocess.call(line, shell=True)
