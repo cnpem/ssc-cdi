fix: fixed normalization in deploy_visualizer
Former-commit-id: 6802f3207a0d53f62737a2a7e82abdfa39d56059 [formerly 4a5edb4f40e9e162f6cbc1549f83d15414bcc24d]

Former-commit-id: 50b61028b626cd3d4790698949d72eaa4d443f59

sscCdi/caterete/cat_restoration.py
@@ -126,7 +126,6 @@ def restoration_CAT(input_dict,method = 'IO'):
         input_dict['DP_center'][1], input_dict['DP_center'][0] = opt540D.mapping540D( input_dict['DP_center'][1], input_dict['DP_center'][0], pi540D.dictionary540D(input_dict["distance"], {'geo': 'nonplanar', 'opt': True, 'mode': 'virtual'} ))
         print(f"Corrected center position: cy={input_dict['DP_center'][0]} cx={input_dict['DP_center'][1]}")
 
-    data_path = input_dict['save_path']
 
     if input_dict['detector'] == '540D':
         detector_size = 3072
@@ -153,12 +152,12 @@ def restoration_CAT(input_dict,method = 'IO'):
         else:
             dic['roi'] = input_dict["detector_ROI_radius"] # integer
 
-        if input_dict["flat_path"] != '':    
+        if input_dict["flatfield"] != '':    
             dic['flat'] = read_hdf5(input_dict["flatfield"])[()][0, 0, :, :] 
         else:
             dic['flat'] = np.ones([detector_size, detector_size])
 
-        if input_dict["mask_path"] != '':    
+        if input_dict["mask"] != '':    
             dic['mask'] = read_hdf5(input_dict["mask"])[()][0, 0, :, :]
         else:
             dic['mask'] = np.zeros([detector_size, detector_size])
@@ -189,6 +188,8 @@ def restoration_CAT(input_dict,method = 'IO'):
 
 
     elif method == "IO":
+    
+        data_path = input_dict['data_path'][0]
 
         """ Get detector geometry from distance """
         geometry, _ = Geometry(input_dict["distance"]*1000,susp=input_dict['suspect_border_pixels'],fill=input_dict['fill_blanks'])

sscCdi/misc.py
@@ -451,7 +451,9 @@ def deploy_visualizer(data,axis=0,title='',cmap='jet',aspect_ratio='',norm="norm
             colornorm=colors.Normalize(vmin=data.min(), vmax=data.max())
         else:
             colornorm=colors.Normalize(vmin=limits[0], vmax=limits[1])
-    
+    elif norm == "LogNorm":
+            colornorm=colors.LogNorm()
+
     def update_imshow(sinogram,figure,subplot,frame_number,axis=0,title="",cmap='gray',norm=None,aspect_ratio=''):
         
         subplot.clear()
@@ -469,7 +471,9 @@ def deploy_visualizer(data,axis=0,title='',cmap='jet',aspect_ratio='',norm="norm
 
         if aspect_ratio != '':
             subplot.set_aspect(aspect_ratio)
-    
+
+        figure.colorbar(matplotlib.cm.ScalarMappable(norm=colornorm, cmap=cmap))
+
     output = widgets.Output()
     
     with output:
@@ -477,7 +481,6 @@ def deploy_visualizer(data,axis=0,title='',cmap='jet',aspect_ratio='',norm="norm
         ax.imshow(np.random.random((4,4)),cmap='gray')
         figure.canvas.draw_idle()
         figure.canvas.header_visible = False
-        figure.colorbar(matplotlib.cm.ScalarMappable(norm=colornorm, cmap=cmap))
         plt.show()   
 
     slider_layout = widgets.Layout(width='25%')

sscCdi/processing/restoration.py
@@ -38,7 +38,8 @@ def restore_CUDA(input_dict,geometry,hdf5_filepaths):
     dic['center']   = input_dict["DP_center"] # [1400,1400]
     dic['flat']     = read_hdf5(input_dict["flatfield"])[()][0, 0, :, :] # numpy.ones([3072, 3072])
     dic['empty']    = np.zeros_like(dic['flat']) # OBSOLETE! empty is not used anymore;
-    
+    dic['daxpy'] = [0,np.zeros([3072,3072])] 
+
     restored_data_info = pi540D.ioSetM_Backward540D( dic )
     output = pi540D.ioGetM_Backward540D( dic, restored_data_info, 11) 
     pi540D.ioCleanM_Backward540D( dic, restored_data_info ) # clean temporary files 
@@ -99,7 +100,15 @@ def read_masks(input_dict):
     else:
         mask = np.zeros(shape)
 
-    return flatfield, mask
+    if input_dict["subtraction_path"] != "":
+        path = input_dict["subtraction_path"]
+        os.system(f"h5clear -s {path}") # gambiarra because file is not closed at the backend!
+        subtraction_mask = np.asarray(h5py.File(input_dict["subtraction_path"], 'r')['entry/data/data']).squeeze().astype(np.float32)
+        subtraction_mask = subtraction_mask * np.squeeze(flatfield) # Apply flatfield
+    else:
+        subtraction_mask = np.zeros(shape)
+
+    return flatfield, mask, subtraction_mask
 
 def corrections_and_restoration(input_dict, DP,geometry, flat, mask, subtraction_mask):
     