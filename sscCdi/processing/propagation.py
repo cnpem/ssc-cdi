Cone beam backpropagation fixed. Fresnel-ptycho ptycho working for parallel and cone-beam case
Former-commit-id: 2fd93717bb71c23beb2511391c606240407148b8 [formerly 84fdd3a17c7221054e25a3998190e1e2943e5699] [formerly 0b12785400120fcf5406f3eb0247dbf8264eed13 [formerly 0b12785400120fcf5406f3eb0247dbf8264eed13 [formerly 74c7a892c7e6b615f247797bfc77b0103fddaeed]]]
Former-commit-id: 0b12785400120fcf5406f3eb0247dbf8264eed13
Former-commit-id: 03bdf152357dc41a4124d2b7f4084378d6e1194e

Former-commit-id: 82aeff8ece91358eb5467e851e22c1c986722145

sscCdi/processing/propagation.py
@@ -63,8 +63,8 @@ def fresnel_propagator_cone_beam(wavefront, wavelength, pixel_size, sample_to_de
     FT = np.fft.fftshift(np.fft.fft2(wavefront))
 
     ny, nx = wavefront.shape
-    fx = np.fft.fftshift(np.fft.fftfreq(nx,d = pixel_size))#*2*np.pi 2*np.pi factor to calculate angular frequencies 
-    fy = np.fft.fftshift(np.fft.fftfreq(ny,d = pixel_size))#*2*np.pi
+    fx = np.fft.fftshift(np.fft.fftfreq(nx,d = pixel_size/M))#*2*np.pi 2*np.pi factor to calculate angular frequencies 
+    fy = np.fft.fftshift(np.fft.fftfreq(ny,d = pixel_size/M))#*2*np.pi
     FX, FY = np.meshgrid(fx,fy)
     # kernel = np.exp(-1j*(z2/M)/(2*K)*(FX**2+FY**2)) # if using angular frequencies. Formula as in Paganin equation 1.28
     kernel = np.exp(-1j*np.pi*wavelength*(z2/M)*(FX**2+FY**2)) # if using standard frequencies. Formula as in Goodman, Fourier Optics, equation 4.21
@@ -73,7 +73,9 @@ def fresnel_propagator_cone_beam(wavefront, wavelength, pixel_size, sample_to_de
 
     if z1 != 0:
         y, x = np.indices(wavefront.shape)
-        wave_cone = wave_parallel * (1/M) * np.exp(1j*gamma_M*K*z2) * np.exp(1j*gamma_M*K*(x**2+y**2)/(2*z2))
+        y = (y - y.shape[0]//2)*pixel_size/M
+        x = (x - x.shape[1]//2)*pixel_size/M
+        wave_cone = wave_parallel * (1/M) #* np.exp(1j*gamma_M*K*z2) * np.exp(1j*gamma_M*K*(x**2+y**2)/(2*z2))
         return wave_cone
     else:
         return wave_parallel