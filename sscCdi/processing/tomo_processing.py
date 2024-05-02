8c21c91efb274d7f98efb3cac1a405ae30c932e6
Yuri Rossi Tonin <yuri.tonin@lnls.br>
2024-05-02 10:50:21 -0300

    Fix of previous commit: added function to get hilbert transform of phase derivative
    Former-commit-id: 149ec7d27f4b846ef952b2f8025b2851679e532d [formerly 7840a04cb3f2d0f24482a4dddc92fe2d9112d34a] [formerly 9b083001d5bb701f8864f4094a67da4572483694 [formerly 9b083001d5bb701f8864f4094a67da4572483694 [formerly d839c8292ed276a21e9743b73ffb3cd011f49151]]]
    Former-commit-id: 9b083001d5bb701f8864f4094a67da4572483694
    Former-commit-id: 42593b1a261d07196685109284758c04864276ec
    
    Former-commit-id: e55a3ae7fa404ff59e46313a52b2baea7c9ceef5

--- sscCdi/processing/tomo_processing.py
+++ sscCdi/processing/tomo_processing.py
@@ -792,6 +792,17 @@ def call_sscRaft(dic, sinogram,save=True):
 
     return reconstruction3D
 
+
+def phase_derivative_hilbert_transform(sinogram,pixel_size=1):
+    """
+    Calculate phase derivative to use it with backprojection (no filter!) 
+    Pixel size in meters
+    """
+    g = np.exp(1j*sinogram) 
+    grad_sinogram = np.angle(g * np.conj(np.roll(g,shift=1,axis=-1)))/pixel_size
+    hilbert = np.imag(scipy.signal.hilbert(grad_sinogram,axis=-1)/(2*np.pi))
+    return hilbert
+
 ####################### PLOT ###########################################
 
 def plot_histograms(recon3D, equalized_tomogram,bins=300):
