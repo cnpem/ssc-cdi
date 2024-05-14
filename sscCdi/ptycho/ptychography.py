38183e85d4fcd6a29163abc335c9b1e9a4e27420
Alan Peixinho <alan.peixinho@lnls.br>
2024-05-14 19:32:17 -0300

    fix missing params
    Former-commit-id: cd1f3046cd32b7b6789c155635e0ca624e2cbc36 [formerly d363cd887e157d9057c3d7efae76fd168c675908] [formerly 98176070152d1fd8bb4936c53abbf6685f2a020d [formerly 98176070152d1fd8bb4936c53abbf6685f2a020d [formerly ae6b8871b8351cbcdec0befe61067783780dcb4b]]]
    Former-commit-id: 98176070152d1fd8bb4936c53abbf6685f2a020d
    Former-commit-id: 0ffd719b28b12f983474dba65535e64bdcd9d6c6
    
    Former-commit-id: 407f47c68dfc438cf38e9cdd771fbf27fce48a29

--- sscCdi/ptycho/ptychography.py
+++ sscCdi/ptycho/ptychography.py
@@ -35,30 +35,30 @@ def call_GCC_ptychography(input_dict,DPs, positions, initial_obj=None, initial_p
 
     positions = positions.astype(np.int32)
     positions = np.roll(positions,shift=1,axis=1) # adjusting to the same standard as GB ptychography
-    
+
     error = np.empty((0,))
-    
+
     inputs = input_dict
     for counter in range(1,1+len(input_dict['algorithms'].keys())):
 
-        inputs['iterations'] = input_dict['algorithms'][str(counter)]['iterations'] 
+        inputs['iterations'] = input_dict['algorithms'][str(counter)]['iterations']
         inputs['distance'] = input_dict["detector_distance"]
-        inputs['regularization_object'] = input_dict['algorithms'][str(counter)]['regularization_object'] 
-        inputs['regularization_probe']  = input_dict['algorithms'][str(counter)]['regularization_probe'] 
-        inputs['step_object']= input_dict['algorithms'][str(counter)]['step_object'] 
-        inputs['step_probe'] = input_dict['algorithms'][str(counter)]['step_probe'] 
+        inputs['regularization_object'] = input_dict['algorithms'][str(counter)]['regularization_object']
+        inputs['regularization_probe']  = input_dict['algorithms'][str(counter)]['regularization_probe']
+        inputs['step_object']= input_dict['algorithms'][str(counter)]['step_object']
+        inputs['step_probe'] = input_dict['algorithms'][str(counter)]['step_probe']
 
         # POSITION CORRECTION. TO BE DONE.
         inputs['position_correction_beta'] = 0 # if 0, does not apply position correction
         inputs['beta'] = 1 # position correction beta value
         inputs['epsilon'] = 0.001 # small value to add to probe/object update denominator
-        # inputs['centralize_probe'] = False # not implemented 
+        # inputs['centralize_probe'] = False # not implemented
 
         if input_dict["algorithms"][str(counter)]['name'] == 'ePIE_python':
             print(f"Calling {input_dict['algorithms'][str(counter)]['iterations'] } iterations of ePIE algorithm...")
-            inputs['friction_object'] = input_dict['algorithms'][str(counter)]['mPIE_friction_obj'] 
-            inputs['friction_probe'] = input_dict['algorithms'][str(counter)]['mPIE_friction_probe'] 
-            inputs['momentum_counter'] = input_dict['algorithms'][str(counter)]['mPIE_momentum_counter'] 
+            inputs['friction_object'] = input_dict['algorithms'][str(counter)]['mPIE_friction_obj']
+            inputs['friction_probe'] = input_dict['algorithms'][str(counter)]['mPIE_friction_probe']
+            inputs['momentum_counter'] = input_dict['algorithms'][str(counter)]['mPIE_momentum_counter']
             obj, probe, algo_error = PIE_multiprobe_loop(DPs, positions,obj[0],probe[0], inputs)
 
         elif input_dict["algorithms"][str(counter)]['name'] == 'RAAR_python':
@@ -98,11 +98,11 @@ def call_GB_ptychography(input_dict,DPs, probe_positions, initial_obj=None, init
 
 
     datapack, sigmask = set_initial_parameters_for_GB_algorithms(input_dict,DPs,probe_positions)
-    
+
     # if initial_obj!=np.ones(1):
     if initial_obj is not None:
         datapack["obj"] = initial_obj
-    
+
     # if initial_probe!=np.ones(1):
     if initial_probe is not None:
         datapack["probe"] = initial_probe
@@ -129,58 +129,65 @@ def call_GB_ptychography(input_dict,DPs, probe_positions, initial_obj=None, init
             break
 
         if algorithm['Name'] == 'GL':
-            datapack = GL(iter      = algorithm['iterations'],
-                                    objbeta   = algorithm['momentum_obj'],
-                                    probebeta = algorithm['momentum_probe'],
-                                    batch     = algorithm['batch'],
-                                    reg_obj   = algorithm['reg_obj'],
-                                    reg_probe = algorithm['reg_probe'],
-                                    sigmask   = sigmask,
-                                    data      = datapack,
-                                    params    = {'device':input_dict["GPUs"]},
-                                    probef1=input_dict['fresnel_number'])
+            datapack = GL(iterations=algorithm['iterations'],
+                          objbeta=algorithm['momentum_obj'],
+                          probebeta=algorithm['momentum_probe'],
+                          batch=algorithm['batch'],
+                          reg_obj=algorithm['reg_obj'],
+                          reg_probe=algorithm['reg_probe'],
+                          sigmask=sigmask,
+                          difpads=datapack['difpads'],
+                          obj=datapack['obj'],
+                          rois=datapack['rois'],
+                          probe=datapack['probe'],
+                          params={'device': input_dict["GPUs"]},
+                          probef1=input_dict['fresnel_number'])
 
         elif algorithm['Name'] == 'positioncorrection':
             datapack['bkg'] = None
-            datapack = PosCorrection(iter       = algorithm['iterations'],
-                                                objbeta   = algorithm.get('momentum_obj', 0.0),
-                                                probebeta = algorithm.get('momentum_probe', 0.0),
-                                                batch     = algorithm['batch'],
-                                                reg_obj   = algorithm['reg_obj'],
-                                                reg_probe = algorithm['reg_probe'],
-                                                sigmask   = sigmask,
-                                                data      = datapack,
-                                                params    = {'device':input_dict["GPUs"]},
-                                                probef1=input_dict['fresnel_number'])
+            datapack = PosCorrection(
+                iterations=algorithm['iterations'],
+                objbeta=algorithm.get('momentum_obj', 0.0),
+                probebeta=algorithm.get('momentum_probe', 0.0),
+                batch=algorithm['batch'],
+                reg_obj=algorithm['reg_obj'],
+                reg_probe=algorithm['reg_probe'],
+                sigmask=sigmask,
+                difpads=datapack['difpads'],
+                obj=datapack['obj'],
+                rois=datapack['rois'],
+                probe=datapack['probe'],
+                params={'device': input_dict["GPUs"]},
+                probef1=input_dict['fresnel_number'])
             corrected_positions = datapack['rois']
 
         elif algorithm['Name'] == 'RAAR':
-            datapack = RAAR(iterations         = algorithm['iterations'],
-                                       probebeta = algorithm['momentum_probe'],
-                                       objbeta = algorithm['momentum_obj'],
-                                       beta = algorithm['beta'],
-                                       batch       = algorithm['batch'],
-                                       reg_obj   = algorithm['reg_obj'],
-                                       reg_probe = algorithm['reg_probe'],
-                                       sigmask     = sigmask,
-                                       rois = datapack['rois'],
-                                       difpads = datapack['difpads'],
-                                       obj        = datapack['obj'],
-                                       probe = datapack['probe'],
-                                       params      = {'device':input_dict["GPUs"]},
-                                       probef1=input_dict['fresnel_number'])
+            datapack = RAAR(iterations=algorithm['iterations'],
+                            probebeta=algorithm['momentum_probe'],
+                            objbeta=algorithm['momentum_obj'],
+                            beta=algorithm['beta'],
+                            batch=algorithm['batch'],
+                            reg_obj=algorithm['reg_obj'],
+                            reg_probe=algorithm['reg_probe'],
+                            sigmask=sigmask,
+                            rois=datapack['rois'],
+                            difpads=datapack['difpads'],
+                            obj=datapack['obj'],
+                            probe=datapack['probe'],
+                            params={'device': input_dict["GPUs"]},
+                            probef1=input_dict['fresnel_number'])
 
         elif algorithm['Name'] == 'PIE':
-            datapack = PIE(iterations = algorithm['iterations'],
-                                     step_obj = algorithm['step_obj'],
-                                     step_probe = algorithm['step_probe'],
-                                     reg_obj = algorithm['reg_obj'],
-                                     reg_probe = algorithm['reg_probe'],
-                                     rois = datapack['rois'],
-                                     difpads = datapack['difpads'],
-                                     obj = datapack['obj'],
-                                     probe = datapack['probe'],
-                                     params = { 'device': input_dict["GPUs"] })
+            datapack = PIE(iterations=algorithm['iterations'],
+                           step_obj=algorithm['step_obj'],
+                           step_probe=algorithm['step_probe'],
+                           reg_obj=algorithm['reg_obj'],
+                           reg_probe=algorithm['reg_probe'],
+                           rois=datapack['rois'],
+                           difpads=datapack['difpads'],
+                           obj=datapack['obj'],
+                           probe=datapack['probe'],
+                           params={'device': input_dict["GPUs"]})
 
         loop_counter += 1
         error = np.concatenate((error,datapack["error"]),axis=0)
@@ -217,7 +224,7 @@ def set_initial_parameters_for_GB_algorithms(input_dict, DPs, probe_positions):
 
         Returns:
             datapack (dictionary)
-        """    
+        """
         print('Creating datapack...') # Set data for Ptycho algorithms
         datapack = {}
         datapack['obj'] = obj
@@ -237,7 +244,7 @@ def set_initial_parameters_for_GB_algorithms(input_dict, DPs, probe_positions):
 
         Returns:
             sigmask (array): 2D-array, same shape of a diffraction pattern, maps the invalid pixels. 0 for negative values
-        """    
+        """
         sigmask = np.ones(DPs[0].shape)
         sigmask[DPs[0] < 0] = 0
         return sigmask
@@ -255,11 +262,11 @@ def set_initial_parameters_for_GB_algorithms(input_dict, DPs, probe_positions):
             probesupp: mask containing probe support
         """
 
-        
+
         print('Setting probe support...')
 
         probe = np.zeros(probe_shape)
-        
+
         if input_dict["probe_support"][0] == "circular":
             radius, center_x, center_y = input_dict["probe_support"][1], input_dict["probe_support"][2], input_dict["probe_support"][3]
 
@@ -274,7 +281,7 @@ def set_initial_parameters_for_GB_algorithms(input_dict, DPs, probe_positions):
         elif input_dict["probe_support"][0] == "cross":
             cross_width_y, border, center_square_side = input_dict['probe_support'][1],input_dict['probe_support'][2],input_dict['probe_support'][3]
             probe[:] = create_cross_mask((probe_shape[1],probe_shape[2]),cross_width_y, border, center_square_side)
-        else: 
+        else:
             sys.exit('Please select the correct probe support: circular or cross')
 
         return probe
@@ -295,7 +302,7 @@ def set_initial_parameters_for_GB_algorithms(input_dict, DPs, probe_positions):
         probe_positions2[:,0] = probe_positions[:,1] # change x and y column order
         probe_positions2[:,1] = probe_positions[:,0]
         return probe_positions2
-    
+
 
     half_size = DPs.shape[-1] // 2
 
@@ -304,16 +311,16 @@ def set_initial_parameters_for_GB_algorithms(input_dict, DPs, probe_positions):
     probe_positions = append_ones(probe_positions)
 
     probe = set_initial_probe(input_dict, (DPs.shape[1], DPs.shape[2]), np.average(DPs, 0)[None] ) # probe initial guess
-    
+
     probe_support = get_probe_support(input_dict, probe.shape)
-    
+
     obj = set_initial_object(input_dict,DPs,probe) # object initial guess
 
     sigmask = set_sigmask(DPs) # mask for invalid pixels
-    background = np.ones(DPs[0].shape) # dummy array 
+    background = np.ones(DPs[0].shape) # dummy array
 
     print(f"Diffraction Patterns: {DPs.shape}\nInitial Object: {obj.shape}\nInitial Probe: {probe.shape}\nProbe Support: {probe_support.shape}\nProbe Positions: {probe_positions.shape}")
-    
+
     datapack = set_datapack(obj, probe, probe_positions, DPs, background, probe_support)     # Set data for Ptycho algorithms:
 
     print(f"Total datapack size: {estimate_memory_usage(datapack['obj'],datapack['probe'],datapack['rois'],datapack['difpads'],datapack['bkg'],datapack['probesupp'])[3]:.2f} GBs")
@@ -353,7 +360,7 @@ def set_initial_probe(input_dict,DP_shape,DPs_avg):
     print('Creating initial probe...')
 
     if isinstance(input_dict['initial_probe'],list): # if no path to file given
-        
+
         type = input_dict['initial_probe'][0]
 
         if type == 'circular':
@@ -418,7 +425,7 @@ def set_initial_object(input_dict,DPs, probe):
             obj =  1 * (np.random.rand(*input_dict["object_shape"]) + 1j*np.random.rand(*input_dict["object_shape"]))
         elif type == 'initialize':
             pass #TODO: implement method from https://doi.org/10.1364/OE.465397
-    elif isinstance(input_dict['initial_obj'],str): 
+    elif isinstance(input_dict['initial_obj'],str):
         if os.path.splitext(input_dict['initial_obj'])[1] == '.hdf5' or os.path.splitext(input_dict['initial_obj'])[1] == '.h5':
             obj = h5py.File(input_dict['initial_obj'],'r')['recon/object'][0] # select first frame of object
         elif os.path.splitext(input_dict['initial_obj'])[1] == '.npy':
@@ -465,26 +472,26 @@ def create_cross_mask(mask_shape, cross_width_y=15, border=3, center_square_side
     Returns:
         mask (array): cross mask
     """
-    
+
     if cross_width_x == 0: cross_width_x = cross_width_y
-    
+
     """ All values in pixels """
-    
+
     # center
     center_row, center_col = mask_shape[0]//2, mask_shape[1]//2
     mask = np.zeros(mask_shape)
     mask[center_row-cross_width_y//2:center_row+cross_width_y//2,:] = 1
     mask[:,center_col-cross_width_x//2:center_col+cross_width_x//2] = 1
-    
+
     # null border
-    mask[0:border,:]  = 0 
-    mask[:,0:border]  = 0 
+    mask[0:border,:]  = 0
+    mask[:,0:border]  = 0
     mask[-border::,:] = 0
     mask[:,-border::] = 0
 
     # center square
     mask[center_row-center_square_side:center_row+center_square_side,center_col-center_square_side:center_col+center_square_side] = 1
-    
+
     return mask
 
 def set_object_pixel_size(input_dict,DP_size):
@@ -505,7 +512,7 @@ def set_object_pixel_size(input_dict,DP_size):
 
     wavelength = wavelength_from_energy(input_dict["energy"])
     input_dict["wavelength"] = wavelength
-    
+
     object_pixel_size = wavelength * input_dict['detector_distance'] / (input_dict["binning"]*input_dict['restored_pixel_size'] * DP_size)
     input_dict["object_pixel"] = object_pixel_size # in meters
 
@@ -541,8 +548,3 @@ def set_object_shape(input_dict, DP_shape, probe_positions):
     input_dict["object_shape"] = (object_shape_y, object_shape_x)
 
     return input_dict
-
-
-
-
-
