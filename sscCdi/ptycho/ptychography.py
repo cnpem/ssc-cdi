8cef9056da5cf13033e9630681aad931a483b792
yurirtonin <yrtonin@gmail.com>
2025-10-09 16:35:53 +0200

    manual fixes to match with status previous to overwrite in last commit

--- sscCdi/ptycho/ptychography.py
+++ sscCdi/ptycho/ptychography.py
@@ -154,13 +154,18 @@ def call_ptychography(input_dict, DPs, positions, initial_obj=None, initial_prob
         input_dict["wavelength"] = wavelength_meters_from_energy_keV(input_dict['energy'])
         print(f"Wavelength = {input_dict['wavelength']*1e9:.3f} nm")
 
-    if "object_pixel" not in input_dict:
-        input_dict["object_pixel"] = calculate_object_pixel_size(input_dict['wavelength'],
-                                                                 input_dict['detector_distance'],
-                                                                 input_dict['detector_pixel_size'],
-                                                                 DPs.shape[1]) # meters
+    if input_dict["regime"] == 'fraunhoffer':
+        if "object_pixel" not in input_dict:
+            input_dict["object_pixel"] = calculate_object_pixel_size(input_dict['wavelength'],
+                                                                    input_dict['detector_distance'], 
+                                                                    input_dict['detector_pixel_size'],
+                                                                    DPs.shape[1]) # meters
+            
+    elif input_dict["regime"] == 'fresnel':
+        if "object_pixel" not in input_dict:
+            input_dict["object_pixel"] = input_dict['detector_pixel_size']
 
-        print(f"Object pixel = {input_dict['object_pixel']*1e9:.2f} nm")
+    print(f"Object pixel = {input_dict['object_pixel']*1e9:.2f} nm")
 
     if input_dict['positions_unit'] is None:
         print("WARNING: assuming positions are in pixels. If not, please set 'positions_unit' in the input dictionary.")
@@ -206,7 +211,7 @@ def call_ptychography(input_dict, DPs, positions, initial_obj=None, initial_prob
 
     if plot:
         print('Plotting final object and probe...')
-        plot_amplitude_and_phase(obj, positions=positions+probe.shape[-1]//2,extent=get_plot_extent_from_positions(positions))
+        plot_amplitude_and_phase(obj, extent=get_plot_extent_from_positions(positions))
         plot_object_spectrum(obj,cmap='gray')
         plot_probe_modes(probe,extent=get_extent_from_pixel_size(probe[0].shape,input_dict["object_pixel"]))
 
@@ -515,8 +520,11 @@ def call_ptychography_engines(input_dict, DPs, positions, initial_obj=None, init
             error_nmse.append(algo_error_mse)
             error_llk.append(algo_error_llk)
 
-            #if algo_inputs["position_correction"] > 0:
-            # corrected_positions = probe_positions
+            if algo_inputs["position_correction"] > 0: # check in every function if position was corrected. Otherwise should be None to avoid plotting
+                corrected_positions = probe_positions
+                corrected_positions = np.roll(corrected_positions, shift=1, axis=1)     # change from (Y,X) back to (X,Y) for visualization
+            else:
+                corrected_positions = None
 
         elif input_dict["algorithms"][str(counter)]['name'] == 'PIE':
             print(f"Calling {input_dict['algorithms'][str(counter)]['iterations'] } iterations of rPIE algorithm...")
@@ -552,21 +560,17 @@ def call_ptychography_engines(input_dict, DPs, positions, initial_obj=None, init
             error_nmse.append(algo_error_mse)
             error_llk.append(algo_error_llk)
 
-            #if algo_inputs["position_correction"] > 0:
-            # corrected_positions = probe_positions
+            if algo_inputs["position_correction"] > 0: # check in every function if position was corrected. Otherwise should be None to avoid plotting
+                corrected_positions = probe_positions
+                corrected_positions = np.roll(corrected_positions, shift=1, axis=1)     # change from (Y,X) back to (X,Y) for visualization
+            else:
+                corrected_positions = None
 
         else:
             sys.exit('Please select a proper algorithm! Selected: ', input_dict["algorithms"][str(counter)]['name'])
 
         if counter != len(input_dict['algorithms'].keys()) and plot == True:
-            plot_amplitude_and_phase(obj, positions=positions+probe.shape[-1]//2,extent=get_plot_extent_from_positions(positions))
-
-    # at this point, corrected_position should be holding either the corrected version of the probe_positions or the original one,
-    # depending on whether algo_inputs["position_correction"]>0 or not,
-    corrected_positions = probe_positions
-
-    # change from (Y,X) back to (X,Y) for visualization
-    corrected_positions = np.roll(corrected_positions, shift=1, axis=1)
+            plot_amplitude_and_phase(obj, extent=get_plot_extent_from_positions(positions))
 
     error_rfactor =  np.concatenate(error_rfactor).ravel()
     error_nmse = np.concatenate(error_nmse).ravel()
